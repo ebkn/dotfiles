d01d499d343b1303de54f0ef4195a196740e3875
ebkn <ktennis.mqekr12@gmail.com>
2018-11-07 00:07:01 +0900

    feat: add docker installation

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -49,6 +49,23 @@ curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >
 sh ~/installer.sh ~/.cache/dein
 rm ~/insdtaller.sh
 
+# Install Docker
+# ref https://docs.docker.com/install/linux/docker-ce/ubuntu
+sudo apt install \
+  apt-transport-https \
+  ca-certificates \
+  software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+  "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) \
+  stable"
+sudo apt install docker-ce
+# ref https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 # Install cli utilities
 sudo apt install \
   tig \
