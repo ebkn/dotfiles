caece31bbaab2d8a23387e4bd32fd88b800f76b0
ebkn <ktennis.mqekr12@gmail.com>
2018-11-06 23:37:58 +0900

    feat: create .zshrc.slim for ubuntu

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -36,7 +36,7 @@ ln -s ~/dotfiles/.tmux.conf .
 sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 rm ~/.zshrc
-ln -s ~/dotfiles/.zshrc .
+ln -s ~/dotfiles/.zshrc.slim .zshrc
 ln -s ~/dotfiles/.zshenv .
 
 # Install vim
