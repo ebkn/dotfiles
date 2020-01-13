df4e929d822e9300c9410fbbbd361264c6d16742
ebkn <ktennis.mqekr12@gmail.com>
2020-01-13 23:40:31 +0900

    feat: add remote branches to gco (git checkout with fzf) command

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -74,7 +74,7 @@ case `uname` in
 
     gco() {
       local branches branch
-      branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+      branches=$(git branch --all | grep -v HEAD) &&
       branch=$(echo "$branches" |
                fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
       git checkout $(echo "$branch" | gsed "s/.* //" | gsed "s#remotes/[^/]*/##")
