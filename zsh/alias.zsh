be3177e13c695540330b1ffe374fe119bc52861d
ebkn <ktennis.mqekr12@gmail.com>
2020-01-14 16:32:06 +0900

    feat: update gco branches at linux

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -98,7 +98,7 @@ case `uname` in
 
     gco() {
       local branches branch
-      branches=$(git for-each-ref --count=30 --sort=-committerdate refs/heads/ --format="%(refname:short)") &&
+      branches=$(git branch --all | grep -v HEAD) &&
       branch=$(echo "$branches" |
                fzf-tmux -d $(( 2 + $(wc -l <<< "$branches") )) +m) &&
       git checkout $(echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
