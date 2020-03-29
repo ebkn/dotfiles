eb8616ce2d42db8e554a1d3449ab08219b94ef31
ebkn <ktennis.mqekr12@gmail.com>
2020-03-29 13:41:13 +0900

    feat: enable some prompt display

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -50,9 +50,9 @@
       status                  # exit code of the last command
       command_execution_time  # duration of the last command
       # background_jobs         # presence of background jobs
-      # direnv                  # direnv status (https://direnv.net/)
+      direnv                  # direnv status (https://direnv.net/)
       virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-      # anaconda                # conda environment (https://conda.io/)
+      anaconda                # conda environment (https://conda.io/)
       # pyenv                   # python environment (https://github.com/pyenv/pyenv)
       # goenv                   # go environment (https://github.com/syndbg/goenv)
       # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
@@ -65,12 +65,12 @@
       rbenv                   # ruby version from rbenv (https://github.com/rbenv/rbenv)
       # rvm                     # ruby version from rvm (https://rvm.io)
       # fvm                     # flutter version management (https://github.com/leoafarias/fvm)
-      # kubecontext             # current kubernetes context (https://kubernetes.io/)
+      kubecontext             # current kubernetes context (https://kubernetes.io/)
       terraform               # terraform workspace (https://www.terraform.io)
       aws                     # aws profile (https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html)
       # aws_eb_env            # aws elastic beanstalk environment (https://aws.amazon.com/elasticbeanstalk/)
-      # azure                 # azure account name (https://docs.microsoft.com/en-us/cli/azure)
-      # gcloud                # google cloud acccount and project (https://cloud.google.com/)
+      azure                 # azure account name (https://docs.microsoft.com/en-us/cli/azure)
+      gcloud                # google cloud acccount and project (https://cloud.google.com/)
       context                 # user@hostname
       # nordvpn                 # nordvpn connection status, linux only (https://nordvpn.com/)
       # ranger                  # ranger shell (https://github.com/ranger/ranger)
