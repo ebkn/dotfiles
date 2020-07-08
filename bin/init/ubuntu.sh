d8e79ff301a598dc77ab31360ea2a95312b65b8a
ebkn <ktennis.mqekr12@gmail.com>
2020-07-08 13:46:35 +0900

    fix: syntax

--- bin/init/ubuntu.sh
+++ bin/init/ubuntu.sh
@@ -97,7 +97,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 printf "\n--- Installing ripgrep ---\n"
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
-$ sudo dpkg -i ripgrep_11.0.2_amd64.deb
+sudo dpkg -i ripgrep_11.0.2_amd64.deb
 
 [ -f ~/.gitignore_global ] && mv ~/.gitignore_global ~/backup/
 ln -s ~/dotfiles/.gitignore_global ~
