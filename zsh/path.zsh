00786e9a278851d06885b0ad4f922a1668be6257
ebkn <ktennis.mqekr12@gmail.com>
2022-07-25 16:10:15 +0900

    feat: android cmdline-tools path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -37,6 +37,7 @@ case `uname` in
     export PATH="$PATH:$ANDROID_HOME/tools"
     export PATH="$PATH:$ANDROID_HOME/tools/bin"
     export PATH="$PATH:$ANDROID_HOME/platform-tools"
+    export PATH="$PATH:$ANDROID_HOME/cmdline-tools/latest/bin"
     export ANDROID_SDK=$ANDROID_HOME
 
     # Deno
