588b2adf54d88573563d6bf6b30cbea20f9c3f06
ebkn <kenichi.ebinuma@gmail.com>
2024-10-21 10:13:41 +0900

    fix: update script

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -123,7 +123,7 @@ case `uname` in
       brew upgrade --cask
       zinit ice proto=ssh depth=1
       zinit update --all
-      nvim +'call dein#update()' +qall
+      nvim +'call dein#update()'
       nvim +'CocUpdate'
       nvim +'TSUpdate'
       go install golang.org/x/tools/...@latest
