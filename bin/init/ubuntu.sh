3affe092b9d63ac9c54c223311ea67191e60d19e
ebkn <kenichi.ebinuma@gmail.com>
2026-02-15 20:39:36 +0900

    fix: commands

--- bin/init/ubuntu.sh
+++ bin/init/ubuntu.sh
@@ -63,10 +63,7 @@ sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
-sudo apt install docker-ce
-# ref https://docs.docker.com/compose/install/
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt install docker-ce docker-compose-plugin
 
 printf "\n--- Installing cli utilities ---\n"
 sudo apt install \
@@ -80,8 +77,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 printf "\n--- Installing ripgrep ---\n"
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/11.0.2/ripgrep_11.0.2_amd64.deb
-sudo dpkg -i ripgrep_11.0.2_amd64.deb
+sudo apt install ripgrep
 
 [ -f ~/.gitignore_global ] && mv ~/.gitignore_global ~/backup/
 ln -s ~/dotfiles/.gitignore_global ~
