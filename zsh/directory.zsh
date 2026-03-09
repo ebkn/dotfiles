722d646d5db96300c9c9da1a0348d0161e314e04
ebkn <kenichi.ebinuma@gmail.com>
2026-03-09 18:19:58 +0900

    feat(tmux): show git context in tab titles
    Display worktree (wt:<branch>) and branch (b:<branch>) in tmux tab
    titles with priority: SSH > worktree > branch > directory basename.
    Git state is detected via chpwd hook and stored as tmux pane options.

--- zsh/directory.zsh
+++ zsh/directory.zsh
@@ -10,7 +10,32 @@ alias -g ......='../../../../..'
 
 # Update tmux window title with unique pane directories
 if [[ -n "$TMUX" ]]; then
+  _tmux_set_git_pane_options() {
+    local git_dir common_dir branch
+    git_dir=$(git rev-parse --git-dir 2>/dev/null) || {
+      tmux set-option -p @git_worktree '' 2>/dev/null
+      tmux set-option -p @git_branch '' 2>/dev/null
+      return
+    }
+    common_dir=$(git rev-parse --git-common-dir 2>/dev/null)
+    branch=$(git branch --show-current 2>/dev/null)
+
+    # Worktree: git-dir differs from common-dir
+    if [[ "$git_dir" != "$common_dir" ]]; then
+      tmux set-option -p @git_worktree "$branch" 2>/dev/null
+    else
+      tmux set-option -p @git_worktree '' 2>/dev/null
+    fi
+
+    # Branch: skip main/develop/empty
+    if [[ -n "$branch" && "$branch" != "main" && "$branch" != "develop" ]]; then
+      tmux set-option -p @git_branch "$branch" 2>/dev/null
+    else
+      tmux set-option -p @git_branch '' 2>/dev/null
+    fi
+  }
   _tmux_update_pane_titles() {
+    _tmux_set_git_pane_options
     tmux-pane-titles 2>/dev/null
   }
   chpwd_functions+=(_tmux_update_pane_titles)
