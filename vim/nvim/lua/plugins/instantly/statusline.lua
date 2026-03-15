144e2a0dc71c9f2ce86f96a5be146ecde92b58a3
ebkn <kenichi.ebinuma@gmail.com>
2026-03-15 11:04:42 +0900

    style(vim): switch lualine theme to everforest

--- vim/nvim/lua/plugins/instantly/statusline.lua
+++ vim/nvim/lua/plugins/instantly/statusline.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       require('lualine').setup({
         options = {
-          theme = 'ayu_mirage',
+          theme = 'everforest',
           colored = true,
           path = 1,
         },
