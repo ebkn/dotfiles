6274372a67b48f9f79f7033f138e1391f73d3da9
ebkn <kenichi.ebinuma@gmail.com>
2026-03-15 10:16:02 +0900

    feat(wezterm): switch color scheme to Night Owl
    Align terminal theme with the Neovim Night Owl colorscheme.

--- wezterm.lua
+++ wezterm.lua
@@ -22,7 +22,7 @@ return {
   font_size = 13,
   line_height = 1.2,
 
-  color_scheme = 'OneDark (base16)',
+  color_scheme = 'Night Owl (Gogh)',
   show_tab_index_in_tab_bar = false,
   tab_max_width = 40,
   window_frame = {
