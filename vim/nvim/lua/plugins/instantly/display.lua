d2b93bac6bbf3e9a1b3c9063a0cce214e8de60c7
ebkn <kenichi.ebinuma@gmail.com>
2026-03-18 11:00:10 +0900

    feat(nvim): add render-markdown.nvim plugin
    Add render-markdown.nvim for in-editor markdown rendering,
    with mini.nvim as an icon dependency.

--- vim/nvim/lua/plugins/instantly/display.lua
+++ vim/nvim/lua/plugins/instantly/display.lua
@@ -46,6 +46,17 @@ return {
     end,
   },
 
+  -- markdown viewer
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+
   -- sticky scroll
   {
     "nvim-treesitter/nvim-treesitter-context",
